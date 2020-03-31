029feca4474c2f1e9d52692b62d2b9e6263c5220
Bruno Rocha <mail.brunorocha@gmail.com>
2020-03-31 14:45:18 -0300

    Refactoring isUsernameAlreadyInUse and isEmailAlreadyInUse methods

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/models/entities/user.entity';
@@ -10,37 +10,18 @@ export class UserService {
   constructor (@InjectRepository(User) private readonly repository: Repository<User>) {}
 
   async createUser (createUserDTO: CreateUserDTO): Promise<User> {
-    // const isUsernameValid = await this.isUsernameAlreadyInUse(createUserDTO.username);
-    const isEmailValid = await this.isEmailAlreadyInUse(createUserDTO.email);
-
-    // if (!isUsernameValid) {
-    //   throw new BadRequestException({ message: `The username ${createUserDTO.username} is already in use` });
-    // }
-
-    if (!isEmailValid) {
-      throw new BadRequestException({ message: `The email ${createUserDTO.email} is already in use` });
-    }
-
     const userEntity = CreateUserDTOToUserEntity(createUserDTO);
     const newUser = await this.repository.save(userEntity);
     return newUser;
   }
 
   async isUsernameAlreadyInUse (username: string): Promise<boolean> {
-    const usersWithUsername = await this.repository.find({ username });
-    if (usersWithUsername.length == 0) {
-      return true
-    }
-
-    return false;
+    const userWithUsername = await this.repository.findOne({ where: { username }});
+    return !userWithUsername;
   }
 
   async isEmailAlreadyInUse (email: string): Promise<boolean> {
-    const usersWithEmail = await this.repository.find({ email });
-    if (usersWithEmail.length == 0) {
-      return true
-    }
-
-    return false;
+    const userWithEmail = await this.repository.findOne({ where: { email }});
+    return !userWithEmail;
   }
 }
