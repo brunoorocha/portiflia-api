a3b65ee8051dbb782c8cb5a2ab0c752bd6b6cbde
Bruno Rocha <mail.brunorocha@gmail.com>
2020-03-31 12:34:20 -0300

    Creating a basic authentication method

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Res, HttpStatus, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserAuthenticationDTO } from 'src/models/dtos/user-authentication.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor (private readonly service: AuthService) {}
 
-  @Get()
-  async getAll (@Res() res) {
-    const users = await this.service.getAll();
-    return res.status(HttpStatus.OK).json(users);
+  @Post()
+  @UsePipes(ValidationPipe)
+  async authenticate (@Res() res, @Body() credentials: UserAuthenticationDTO) {
+    const user = await this.service.authenticate(credentials);
+    return res.status(HttpStatus.OK).json({ token: 'Bearer <user_token>', user });
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/models/entities/user.entity';
 import { Repository } from 'typeorm';
+import { UserAuthenticationDTO } from 'src/models/dtos/user-authentication.dto';
 
 @Injectable()
 export class AuthService {
   constructor (@InjectRepository(User) private readonly repository: Repository<User>) {}
 
-  async getAll (): Promise<User[]> {
-    const users = await this.repository.find();
-    return users;
+  async authenticate (userAuthenticationDTO: UserAuthenticationDTO): Promise<User> {
+    const userForUsername = await this.repository.findOne({ where: { username: userAuthenticationDTO.username }});
+    if (!userForUsername) {
+      throw new BadRequestException({ message: `There's no user with username ${userAuthenticationDTO.username}.` });
+    }
+
+    if (userForUsername.password != userAuthenticationDTO.password) {
+      throw new BadRequestException({ message: `Your password its wrong.` });
+    }
+
+    return userForUsername;
   }
 }
