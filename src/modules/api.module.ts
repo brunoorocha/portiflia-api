f9d86b97edfe9e86807ede7bb0fa871af7cf2814
Bruno Rocha <mail.brunorocha@gmail.com>
2020-04-01 17:01:32 -0300

    Removing images module

--- src/modules/api.module.ts
+++ src/modules/api.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProjectModule } from './project/project.module';
-import { ImagesModule } from './images/images.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ProjectModule, ImagesModule],
-  exports: [AuthModule, UserModule, ProjectModule, ImagesModule]
+  imports: [AuthModule, UserModule, ProjectModule],
+  exports: [AuthModule, UserModule, ProjectModule]
 })
 export class ApiModule {}
